3b10a2857f494dc985dacd488fc0e09c4f32f98e
thevks <techikida@gmail.com>
2021-11-14 12:27:19 +0530

    Bit Manipulation Questions

--- data_structures/graphs/Graph.java
+++ data_structures/graphs/Graph.java
@@ -1,47 +1,23 @@
-// BFS algorithm in Java
+// DFS algorithm in Java
 
 import java.util.*;
 
-public class Graph {
-  private int V;
-  private LinkedList<Integer> adj[];
+class Graph {
+  private LinkedList<Integer> adjLists[];
+  private boolean visited[];
 
-  // Create a graph
-  Graph(int v) {
-    V = v;
-    adj = new LinkedList[v];
-    for (int i = 0; i < v; ++i)
-      adj[i] = new LinkedList();
+  // Graph creation
+  Graph(int vertices) {
+    adjLists = new LinkedList[vertices];
+    visited = new boolean[vertices];
+
+    for (int i = 0; i < vertices; i++)
+      adjLists[i] = new LinkedList<Integer>();
   }
 
-  // Add edges to the graph
-  void addEdge(int v, int w) {
-    adj[v].add(w);
-  }
-
-  // BFS algorithm
-  void BFS(int s) {
-
-    boolean visited[] = new boolean[V];
-
-    LinkedList<Integer> queue = new LinkedList();
-
-    visited[s] = true;
-    queue.add(s);
-
-    while (queue.size() != 0) {
-      s = queue.poll();
-      System.out.print(s + " ");
-
-      Iterator<Integer> i = adj[s].listIterator();
-      while (i.hasNext()) {
-        int n = i.next();
-        if (!visited[n]) {
-          visited[n] = true;
-          queue.add(n);
-        }
-      }
-    }
+  // Add edges
+  void addEdge(int src, int dest) {
+    adjLists[src].add(dest);
   }
 
   // DFS algorithm
@@ -57,18 +33,64 @@ public class Graph {
     }
   }
 
+  // BFS algorithm
+  void BFS(int s) {
+
+    boolean visited[] = new boolean[V];
+
+    LinkedList<Integer> queue = new LinkedList();
+
+    visited[s] = true;
+    queue.add(s);
+
+    while (queue.size() != 0) { 
+      s = queue.poll();
+      System.out.print(s + " ");// BFS algorithm
+      void BFS(int s) {
+    
+        boolean visited[] = new boolean[V];
+    
+        LinkedList<Integer> queue = new LinkedList();
+    
+        visited[s] = true;
+        queue.add(s);
+    
+        while (queue.size() != 0) {
+          s = queue.poll();
+          System.out.print(s + " ");
+    
+          Iterator<Integer> i = adj[s].listIterator();
+          while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n]) {
+              visited[n] = true;
+              queue.add(n);
+            }
+          }
+        }
+      }
+
+      Iterator<Integer> i = adj[s].listIterator();
+      while (i.hasNext()) {
+        int n = i.next();
+        if (!visited[n]) {
+          visited[n] = true;
+          queue.add(n);
+        }
+      }
+    }
+  }
+
   public static void main(String args[]) {
     Graph g = new Graph(4);
 
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 2);
-    g.addEdge(2, 0);
     g.addEdge(2, 3);
-    g.addEdge(3, 3);
 
-    System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
+    System.out.println("Following is Depth First Traversal");
 
-    g.BFS(2);
+    g.DFS(2);
   }
 }
