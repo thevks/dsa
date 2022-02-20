d30fff5e94bb0f46fc17da99302d60af2e457840
thevks <techikida@gmail.com>
2022-02-20 23:26:49 +0530

    Coins Change Problem

--- questions/dynamic_programming/1_coins_change.cpp
+++ questions/dynamic_programming/1_coins_change.cpp
@@ -1,72 +1,46 @@
-// { Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-
-class Solution
-{
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
+class Solution {
+  public:
+    long long int dp[1001][1001];
     
-    int dp[1001][1001];
-    
-    int solve(int W, int wt[], int val[], int n)
+    long long int solve(int S[], int m, int n)
     {
-        if (W == 0 or n < 0) return 0;
+        if (n == 0) return 1;
         
-        if (dp[n][W] == -1) return dp[n][W];
+        if (n < 0 || m < 0) return 0;
         
-        //Skip if weight to be considered is more than the expected
-        if (wt[n] > W)
-            return dp[n][W] = solve(W, wt, val, n-1);
+        if (dp[m][n] != -1) return dp[m][n];
         
-        int include = val[n] + solve(W-wt[n], wt, val, n-1);
-        int exclude = solve(W, wt, val, n-1);
+        dp[m][n] = solve(S, m, n-S[m]) + solve(S, m-1, n);
         
-        dp[n][W] = max(include, exclude);
-        
-        return dp[n][W];
+        return dp[m][n];
     }
     
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-        for (int i=0; i<n; i++)
-            for (int j=0; j<W; j++)
+    long long int count(int S[], int m, int n) 
+    {
+        dp[m][n+1];
+        
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
                 dp[i][j] = -1;
-            
-        return solve(W, wt, val, n-1);    
+          
+        return solve(S, m-1, n);
     }
 };
 
-// { Driver Code Starts.
-
-int main()
- {
-    //taking total testcases
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-        //reading number of elements and weight
-        int n, w;
-        cin>>n>>w;
-        
-        int val[n];
-        int wt[n];
-        
-        //inserting the values
-        for(int i=0;i<n;i++)
-            cin>>val[i];
-        
-        //inserting the weights
-        for(int i=0;i<n;i++)
-            cin>>wt[i];
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int arr[m];
+        for (int i = 0; i < m; i++) cin >> arr[i];
         Solution ob;
-        //calling method knapSack()
-        cout<<ob.knapSack(w, wt, val, n)<<endl;
-        
+        cout << ob.count(arr, m, n) << endl;
     }
-	return 0;
-}  // } Driver Code Ends
+
+    return 0;
+}  
